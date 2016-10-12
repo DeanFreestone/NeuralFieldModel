feb0fc2ce0630c5bd8e9f0af7f7cba26af3054a4
Miao Cao <caomiao89@gmail.com>
2016-10-12 17:52:18 +1100

    update

--- Analysis/ComputeStateVectorXt_Check_Analytic.m
+++ Analysis/ComputeStateVectorXt_Check_Analytic.m
@@ -12,7 +12,7 @@ close all
 
 
 % parameters to create a 2-D cortical surface
-SpaceMin = -10; SpaceMax = 10; NPoints = 501;
+SpaceMin = -10; SpaceMax = 10; NPoints = 201;
 x = linspace(SpaceMin, SpaceMax, NPoints);
 stepSize = x(2)-x(1);
 [X, Y] = meshgrid(x, x);
@@ -99,20 +99,33 @@ ks = 1- Ts*(1/tau); % time constant parameter
 errorPart = zeros(NPoints, NPoints); % set error part to zero for now
 %% integral part
 
+
+% initialise integral part
+integralPart = zeros(NPoints, NPoints);
 % firing rate function
 firingRate_v_t = 1 ./ ( 1 + exp(slope_sigmoidal*(v0 - v_t)));
 
-% connectivity kernel, a sum of three gaussian kernels
-theta = [10, -8, 0.5]';
-sigma = [0.6 0.8 2];
-for n = 1 : 3
-    gaussians(:,:, n) = Define2DGaussian_AnisotropicKernel(0, 0, [sigma(n) 0; 0 sigma(n)], 201, SpaceMin, SpaceMax) * theta(n);
+% integral. convolution or integral
+for m = 1 : NPoints
+    for n = 1 : NPoints
+        r = [X(m, n), Y(m, n)]; % location r vector
+        
+        % define connectivity kernel at location r
+        % connectivity kernel, a sum of three gaussian kernels
+        theta = [10, -8, 0.5]';
+        sigma = [0.6 0.8 2];
+        for p = 1 : 3
+            gaussians(:,:, p) = Define2DGaussian_AnisotropicKernel(r(1), r(2), [sigma(p) 0; 0 sigma(p)], NPoints, SpaceMin, SpaceMax) * theta(p);
+        end
+        w = squeeze(sum(gaussians, 3));
+        
+        integralPart = integralPart + w.*v_t;
+    end
 end
 
-w = squeeze(sum(gaussians, 3));
 
-% integral. convolution or integral
-integralPart = 0;
+% average??
+
 %% v(t+1)
 
 v_tplus1 = ks * v_t + Ts * integralPart + errorPart;
