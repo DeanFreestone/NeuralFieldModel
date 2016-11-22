58b02c9a6fe0a0f76f50567d27065f06770a2440
Miao Cao <caomiao89@gmail.com>
2016-11-22 17:07:58 +1100

    update

--- Analysis/Check_ChangeConvolutionOrder.m
+++ Analysis/Check_ChangeConvolutionOrder.m
@@ -16,26 +16,35 @@ close all
 
 
 
-SpaceMin = -10; SpaceMax = 10; NPoints = 401;
+SpaceMin = -10; SpaceMax = 10; NPoints = 301;
+
 % random data, fData
 x = linspace(SpaceMin, SpaceMax, NPoints);
+
 stepSize = x(2)-x(1);
+
 [X, Y] = meshgrid(x, x);
+
 fData = randn(NPoints);
 
 % Define phi Gaussian basis function
 %
-mu_phi = [1 1];                   % center of basis function
-sigma_phi = [1 0; 0 1];           % variance-covariance matrix of phi
+mu_phi = [0 0];                   % center of basis function
 
-phi = Define2DGaussian_3(mu_phi(1), mu_phi(2), sigma_phi(1,1), 0, NPoints, SpaceMin, SpaceMax);
-% phi = Define2DGaussian(mu_phi(1), mu_phi(2), sigma_phi(1,1), 0, NPoints, SpaceMin, SpaceMax);
+sigma_phi = [1 0.2; 0.2 1];           % variance-covariance matrix of phi
+
+
+phi = Define2DGaussian_AnisotropicKernel(mu_phi(1), mu_phi(2), sigma_phi, NPoints, SpaceMin, SpaceMax);
+% phi = Define2DGaussian_3(mu_phi(1), mu_phi(2), sigma_phi(1,1), 0, NPoints, SpaceMin, SpaceMax);
+
 
 % Define psi Gaussian kernel
-mu_psi = [-1 -1];              % centre of basis function
-sigma_psi = [1.5 0; 0 1.5];        % variance-covariance matrix of phi
-psi = Define2DGaussian_3(mu_psi(1), mu_psi(2), sigma_psi(1,1), 0, NPoints, SpaceMin, SpaceMax);
-% psi = Define2DGaussian(mu_psi(1), mu_psi(2), sigma_psi(1,1), 0, NPoints, SpaceMin, SpaceMax);
+mu_psi = [0 0];              % centre of basis function
+
+sigma_psi = [2 1; 1 2];        % variance-covariance matrix of phi
+
+psi = Define2DGaussian_AnisotropicKernel(mu_psi(1), mu_psi(2), sigma_psi, NPoints, SpaceMin, SpaceMax);
+% psi = Define2DGaussian_3(mu_psi(1), mu_psi(2), sigma_psi(1,1), 0, NPoints, SpaceMin, SpaceMax);
 
 
 % psi = mvnpdf([X(:) Y(:)], mu_psi, sigma_psi);
@@ -48,17 +57,20 @@ psi = Define2DGaussian_3(mu_psi(1), mu_psi(2), sigma_psi(1,1), 0, NPoints, Space
 
 
 % E1 Phi@(Psi*fData), @ is defined as inner product of two functions (matrices)
+
+% convolution
 convE1 = conv2(psi, fData, 'same');
-E1 = sum(sum(phi.*convE1, 2), 1);
+
+% integral
+E1 = sum(sum(phi.*convE1*stepSize^2, 2), 1);
+
 
 % E2 (Phi*Psi)@fData
+
+% convolution
 convE2 = conv2(phi, psi, 'same');
-E2 = sum(sum(convE2.*fData, 2), 1);
 
-conv2_convPhiPsi = convE2 * stepSize^2;
-%% Plot
-% ~~~~~~~~~~~~~~~
+% integral
+E2 = sum(sum(convE2.*fData*stepSize^2, 2), 1);
 
-
-
-figure, surf(X, Y, convE1); colorbar; title('Psi convolves with random data');
+residual = E1 - E2
